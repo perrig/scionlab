9ceb11b8956381e5509bf1b2eb4f640d5e1fdd99
Adrian Perrig <perrig@gmail.com>
2017-11-21 07:46:02 +0100

    basic SCION version working

--- bwtester/bwtestclient/bwtestclient.go
+++ bwtester/bwtestclient/bwtestclient.go
@@ -5,22 +5,19 @@
 package main
 
 import (
-	// "bytes"
-	// "crypto/aes"
 	"crypto/rand"
-	// "encoding/binary"
-	// "encoding/gob"
 	"flag"
 	"fmt"
-	"net"
-	// "io/ioutil"
+	"strconv"
+	"strings"
 	"time"
 
-	// "github.com/netsec-ethz/scion/go/lib/snet"
-	. "github.com/perrig/scionlab/bwtester/bwtestlib"
+	"github.com/netsec-ethz/scion/go/lib/snet"
+	// . "github.com/perrig/scionlab/bwtester/bwtestlib"
+	. "adrian/bwtestlib"
 )
 
-func prepareAESKey() ([]byte) {
+func prepareAESKey() []byte {
 	key := make([]byte, 16)
 	n, err := rand.Read(key)
 	Check(err)
@@ -30,44 +27,116 @@ func prepareAESKey() ([]byte) {
 	return key
 }
 
-func main() {
-	// Address of client control channel (CC)
-	// Todo: check if port already in use, then pick a different one
-	clientCCAddr, err := net.ResolveUDPAddr( "udp", "127.0.0.1:18008" )
-	Check( err )
-	// Address of server control channel (CC)
-	serverCCAddr, err := net.ResolveUDPAddr( "udp", "127.0.0.1:18007" )
-	Check( err )
-	// Address of client data channel (DC)
-	clientDCAddr, err := net.ResolveUDPAddr( "udp", "127.0.0.1:18010" )
-	Check( err )
-	// Address of server data channel (DC)
-	serverDCAddr, err := net.ResolveUDPAddr( "udp", "127.0.0.1:18009" )
-	Check( err )
+func printUsage() {
+	fmt.Println("imagefetcher -c ClientSCIONAddress -s ServerSCIONAddress")
+	fmt.Println("The SCION address is specified as ISD-AS,[IP Address]:Port")
+	fmt.Println("Example SCION address 1-1011,[192.33.93.166]:42002")
+}
 
-	// Control channel connection
-	CCConn, err := net.DialUDP( "udp", clientCCAddr, serverCCAddr )
-	Check( err )
+func main() {
+	var (
+		clientCCAddrStr string
+		serverCCAddrStr string
+		clientISDASIP   string
+		serverISDASIP   string
+		clientPort      int
+		serverPort      int
+		// Address of client control channel (CC)
+		clientCCAddr *snet.Addr
+		// Address of server control channel (CC)
+		serverCCAddr *snet.Addr
+		// Control channel connection
+		CCConn *snet.Conn
+
+		// clientDCAddrStr string
+		// serverDCAddrStr string
+		// Address of client data channel (DC)
+		clientDCAddr *snet.Addr
+		// Address of server data channel (DC)
+		serverDCAddr *snet.Addr
+		// Data channel connection
+		DCConn *snet.Conn
+
+		err error
+	)
+
+	flag.StringVar(&clientCCAddrStr, "c", "", "Client SCION Address")
+	flag.StringVar(&serverCCAddrStr, "s", "", "Server SCION Address")
+
+	flag.Parse()
+
+	// Create SCION UDP socket
+	if len(clientCCAddrStr) > 0 {
+		clientCCAddr, err = snet.AddrFromString(clientCCAddrStr)
+		Check(err)
+	} else {
+		printUsage()
+		Check(fmt.Errorf("Error, client address needs to be specified with -c"))
+	}
+	if len(serverCCAddrStr) > 0 {
+		serverCCAddr, err = snet.AddrFromString(serverCCAddrStr)
+		Check(err)
+	} else {
+		printUsage()
+		Check(fmt.Errorf("Error, server address needs to be specified with -s"))
+	}
+
+	sciondAddr := "/run/shm/sciond/sd" + strconv.Itoa(clientCCAddr.IA.I) + "-" + strconv.Itoa(clientCCAddr.IA.A) + ".sock"
+	dispatcherAddr := "/run/shm/dispatcher/default.sock"
+	snet.Init(clientCCAddr.IA, sciondAddr, dispatcherAddr)
+	CCConn, err = snet.DialSCION("udp4", clientCCAddr, serverCCAddr)
+	Check(err)
+	fmt.Println("clientCCAddr -> serverCCAddr", clientCCAddr, "->", serverCCAddr)
+
+	ci := strings.LastIndex(serverCCAddrStr, ":")
+	if ci < 0 {
+		// This should never happen, an error would have been much earlier detected
+		Check(fmt.Errorf("Malformed server address"))
+	}
+	serverISDASIP = serverCCAddrStr[:ci]
+	serverPort, err = strconv.Atoi(serverCCAddrStr[ci+1:])
+	Check(err)
+	fmt.Println("serverISDASIP:", serverISDASIP)
+	fmt.Println("serverPort:", serverPort)
+
+	ci = strings.LastIndex(clientCCAddrStr, ":")
+	if ci < 0 {
+		// This should never happen, an error would have been much earlier detected
+		Check(fmt.Errorf("Malformed client address"))
+	}
+	clientISDASIP = clientCCAddrStr[:ci]
+	clientPort, err = strconv.Atoi(clientCCAddrStr[ci+1:])
+	Check(err)
+	fmt.Println("clientISDASIP:", clientISDASIP)
+	fmt.Println("clientPort:", clientPort)
+
+	// Address of client data channel (DC)
+	clientDCAddr, err = snet.AddrFromString(clientISDASIP + ":" + strconv.Itoa(clientPort+1))
+	Check(err)
+	// Address of server data channel (DC)
+	serverDCAddr, err = snet.AddrFromString(serverISDASIP + ":" + strconv.Itoa(serverPort+1))
+	Check(err)
 
 	// Data channel connection
-	DCConn, err := net.DialUDP( "udp", clientDCAddr, serverDCAddr )
-	Check( err )
+	DCConn, err = snet.DialSCION("udp4", clientDCAddr, serverDCAddr)
+	Check(err)
+	fmt.Println("clientDCAddr -> serverDCAddr", clientDCAddr, "->", serverDCAddr)
 
 	// Prepare arguments
 	clientKey := prepareAESKey()
 	serverKey := prepareAESKey()
 
 	clientBwp := BwtestParameters{time.Second * 3,
-		1000,
-		500000,
+		100,
+		5,
 		clientKey,
-		18010}
+		uint16(clientPort + 1)}
 
 	serverBwp := BwtestParameters{time.Second * 3,
-		1000,
-		500000,
+		100,
+		5,
 		serverKey,
-		18009}
+		uint16(serverPort + 1)}
 
 	go HandleDCConnReceive(&serverBwp, DCConn)
 
@@ -75,10 +144,10 @@ func main() {
 	n := EncodeBwtestParameters(&clientBwp, pktbuf)
 	l := n
 	n = EncodeBwtestParameters(&serverBwp, pktbuf[n:])
-	l = l+n
+	l = l + n
 
 	_, err = CCConn.Write(pktbuf[:l])
-	Check( err )
+	Check(err)
 
 	n, err = CCConn.Read(pktbuf)
 	Check(err)
--- bwtester/bwtestserver/bwtestserver.go
+++ bwtester/bwtestserver/bwtestserver.go
@@ -7,56 +7,104 @@ package main
 import (
 	"flag"
 	"fmt"
-	"net"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/netsec-ethz/scion/go/lib/snet"
-	. "github.com/perrig/scionlab/bwtester/bwtestlib"
+	// . "github.com/perrig/scionlab/bwtester/bwtestlib"
+	. "adrian/bwtestlib"
 )
 
-func main() {
-	
+func printUsage() {
+	fmt.Println("bwtestserver -s ServerSCIONAddress")
+	fmt.Println("The SCION address is specified as ISD-AS,[IP Address]:Port")
+	fmt.Println("Example SCION address 1-1,[127.0.0.1]:42002")
+}
 
-	serverUDPaddress, err := net.ResolveUDPAddr( "udp", ":18007" )
-	Check( err )
-	CCConn, err := net.ListenUDP( "udp", serverUDPaddress )
-	Check( err )
+func main() {
+	var (
+		serverCCAddrStr string
+		serverCCAddr    *snet.Addr
+		err             error
+		CCConn          *snet.Conn
+	)
+
+	// Fetch arguments from command line
+	flag.StringVar(&serverCCAddrStr, "s", "", "Server SCION Address")
+	flag.Parse()
+
+	// Create the SCION UDP socket
+	if len(serverCCAddrStr) > 0 {
+		serverCCAddr, err = snet.AddrFromString(serverCCAddrStr)
+		if err != nil {
+			printUsage()
+			Check(err)
+		}
+	} else {
+		printUsage()
+		Check(fmt.Errorf("Error, server address needs to be specified with -s"))
+	}
+
+	sciondAddr := "/run/shm/sciond/sd" + strconv.Itoa(serverCCAddr.IA.I) + "-" + strconv.Itoa(serverCCAddr.IA.A) + ".sock"
+	dispatcherAddr := "/run/shm/dispatcher/default.sock"
+	snet.Init(serverCCAddr.IA, sciondAddr, dispatcherAddr)
+
+	ci := strings.LastIndex(serverCCAddrStr, ":")
+	if ci < 0 {
+		// This should never happen, an error would have been much earlier detected
+		Check(fmt.Errorf("Malformed server address"))
+	}
+	serverISDASIP := serverCCAddrStr[:ci]
+	// fmt.Println("serverISDASIP:", serverISDASIP)
+
+	CCConn, err = snet.ListenSCION("udp4", serverCCAddr)
+	Check(err)
 
 	receivePacketBuffer := make([]byte, 2500)
 	sendPacketBuffer := make([]byte, 2500)
 	for {
 		// Handle client requests
-		
-		n, clientUDPaddress, err := CCConn.ReadFromUDP( receivePacketBuffer )
+
+		n, clientCCAddr, err := CCConn.ReadFrom(receivePacketBuffer)
 		if err != nil {
 			// Todo: check error in detail, but for now simply continue
 			continue
 		}
-		
+		// fmt.Println("clientCCAddr:", clientCCAddr.String())
+
 		clientBwp, n := DecodeBwtestParameters(receivePacketBuffer[:n])
 		// fmt.Println(clientBwp)
 
 		serverBwp, n := DecodeBwtestParameters(receivePacketBuffer[n:])
 		// fmt.Println(serverBwp)
 
-		// Data channel connection
+		clientCCAddrStr := clientCCAddr.String()
+		ci := strings.LastIndex(clientCCAddrStr, ":")
+		if ci < 0 {
+			// This should never happen
+			Check(fmt.Errorf("Malformed client address"))
+		}
+		clientISDASIP := clientCCAddrStr[:ci]
 
 		// Address of client data channel (DC)
-		clientDCAddr, err := net.ResolveUDPAddr( "udp", "127.0.0.1:" + strconv.Itoa(int(clientBwp.Port)))
-		Check( err )
+		ca := clientISDASIP + ":" + strconv.Itoa(int(clientBwp.Port))
+		clientDCAddr, err := snet.AddrFromString(ca)
+		Check(err)
 		// Address of server data channel (DC)
-		serverDCAddr, err := net.ResolveUDPAddr( "udp", "127.0.0.1:" + strconv.Itoa(int(serverBwp.Port)))
-		Check( err )
+		serverDCAddr, err := snet.AddrFromString(serverISDASIP + ":" + strconv.Itoa(int(serverBwp.Port)))
+		Check(err)
 
-		DCConn, err := net.DialUDP( "udp", serverDCAddr, clientDCAddr )
-		Check( err )
+		// Data channel connection
+		DCConn, err := snet.DialSCION("udp4", serverDCAddr, clientDCAddr)
+		Check(err)
+		fmt.Println("serverDCAddr -> clientDCAddr", serverDCAddr, "->", clientDCAddr)
 
 		go HandleDCConnReceive(clientBwp, DCConn)
 		go HandleDCConnSend(serverBwp, DCConn)
 
 		sendPacketBuffer[0] = byte(1)
-		n, err = CCConn.WriteTo(sendPacketBuffer[:1], clientUDPaddress)
+		n, err = CCConn.WriteTo(sendPacketBuffer[:1], clientCCAddr)
 		Check(err)
 
 		// Wait for a very generous amount of time
