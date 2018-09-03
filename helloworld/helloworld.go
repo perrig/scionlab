55558f6cfcb90ce2c000aeadd472b8529480db04
Juan A. Garcia Pardo <juagargi@gmail.com>
2018-09-03 12:14:50 +0200

    remote is a parameter. Also simplify

--- helloworld/helloworld.go
+++ helloworld/helloworld.go
@@ -18,13 +18,13 @@ import (
 	"flag"
 	"fmt"
 	"os"
-	"time"
 
 	"github.com/scionproto/scion/go/lib/sciond"
 	"github.com/scionproto/scion/go/lib/snet"
 	"github.com/scionproto/scion/go/lib/spath"
 )
 
+// Check just ensures the error is nil, or complains and quits
 func Check(e error) {
 	if e != nil {
 		fmt.Println("Fatal error. Exiting.", "err", e)
@@ -33,35 +33,39 @@ func Check(e error) {
 }
 
 func main() {
-	var (
-		err             error
-		clientCCAddrStr string
-		clientCCAddr    *snet.Addr
-	)
+	var err error
+	var clientCCAddrStr string
+	var serverCCAddrStr string
 	dispatcherPath := "/run/shm/dispatcher/default.sock"
-	serverCCAddrStr := "17-ffaa:0:1101,[127.0.0.1]:1122"
-
-	flag.StringVar(&clientCCAddrStr, "local", "", "Client SCION Address")
+	// get local and remote addresses from program arguments:
+	flag.StringVar(&clientCCAddrStr, "local", "", "Local SCION Address (e.g. 17-ffaa:1:1,[127.0.0.1]:0)")
+	flag.StringVar(&serverCCAddrStr, "remote", "", "Remote SCION Address (e.g. 17-ffaa:1:1,[127.0.0.1]:12345)")
 	flag.Parse()
-	if len(clientCCAddrStr) > 0 {
-		clientCCAddr, err = snet.AddrFromString(clientCCAddrStr)
-		Check(err)
-	} else {
-		Check(fmt.Errorf("Error, client address needs to be specified with -local"))
+	if len(clientCCAddrStr) == 0 {
+		Check(fmt.Errorf("Error, local address needs to be specified with -local"))
 	}
-	sciondPath := sciond.GetDefaultSCIONDPath(nil)
-	err = snet.Init(clientCCAddr.IA, sciondPath, dispatcherPath)
+	if len(serverCCAddrStr) == 0 {
+		Check(fmt.Errorf("Error, remote address needs to be specified with -remote"))
+	}
+	clientCCAddr, err := snet.AddrFromString(clientCCAddrStr)
 	Check(err)
 	serverCCAddr, err := snet.AddrFromString(serverCCAddrStr)
 	Check(err)
-
+	// get the daemon socket file path:
+	sciondPath := sciond.GetDefaultSCIONDPath(nil)
+	// initialize SCION
+	err = snet.Init(clientCCAddr.IA, sciondPath, dispatcherPath)
+	Check(err)
+	// query paths from here to there:
 	pathMgr := snet.DefNetwork.PathResolver()
 	pathSet := pathMgr.Query(clientCCAddr.IA, serverCCAddr.IA)
 	if len(pathSet) == 0 {
 		Check(fmt.Errorf("No paths"))
 	}
+	// print all paths. Also pick one path. Here we chose the path with less hops:
 	i := 0
 	minLength, argMinPath := 999, (*sciond.PathReplyEntry)(nil)
+	fmt.Println("Available paths:")
 	for _, path := range pathSet {
 		fmt.Printf("[%2d] %d %s\n", i, len(path.Entry.Path.Interfaces)/2, path.Entry.Path.String())
 		if len(path.Entry.Path.Interfaces) < minLength {
@@ -70,24 +74,19 @@ func main() {
 		}
 		i++
 	}
-	fmt.Println(argMinPath.Path.String())
-
-	if serverCCAddr.IA.Eq(clientCCAddr.IA) {
-		return
-	}
+	fmt.Println("Chosen path:", argMinPath.Path.String())
+	// we need to copy the path to the destination (destination is the whole selected path)
 	serverCCAddr.Path = spath.New(argMinPath.Path.FwdPath)
 	serverCCAddr.Path.InitOffsets()
 	serverCCAddr.NextHopHost = argMinPath.HostInfo.Host()
 	serverCCAddr.NextHopPort = argMinPath.HostInfo.Port
-
+	// get a connection object using that path:
 	conn, err := snet.DialSCION("udp4", clientCCAddr, serverCCAddr)
 	Check(err)
 	defer conn.Close()
-	err = conn.SetWriteDeadline(time.Now().Add(time.Second * 20))
-	Check(err)
+	// when we have set our connection up, we just use it. Write some content:
+	// you could visualize the packet(s) with e.g. sudo tcpdump -i any -n -A -w - |grep -a 'hello world'
 	nBytes, err := conn.Write([]byte("hello world"))
 	Check(err)
-	fmt.Println(nBytes)
-	fmt.Println("Done.")
-	// catch the packet with: sudo tcpdump -i tun0 -n -A -w - |grep -a 'hello'
+	fmt.Printf("Done. Wrote %d bytes.\n", nBytes)
 }
